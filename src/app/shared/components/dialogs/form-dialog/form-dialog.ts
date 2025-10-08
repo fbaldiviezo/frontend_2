468dbcd7345d577e60cdba427a319970c6ff12b1
fbdjk <fbaldiviezojerez@gmail.com>
2025-10-08 14:13:20 -0400

    dialogs create

---
+++ src/app/shared/components/dialogs/form-dialog/form-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-dialog',
+  imports: [],
+  templateUrl: './form-dialog.html',
+  styleUrl: './form-dialog.css'
+})
+export class FormDialog {
+
+}
